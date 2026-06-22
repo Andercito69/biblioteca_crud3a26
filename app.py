ecb40fc64409fd440b3bd766ae75532c01930059
Adercito69 <ulicarlo.2109@gmail.com>
2026-06-22 10:33:52 -0600

    Mejorando la consulta de libros

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from dao.libro_dao import LibroDAO
 from models.libro import Libro
 
-def main():
-    try:    
+
+def ver_libros():
+ try:    
         libro_dao = LibroDAO()
 
         libros = libro_dao.obtener_todos()
@@ -21,9 +22,49 @@ def main():
                 )
                 print("----------------------------------------------")
         print("\nConexion exitosa a la base de datos.")
-    except Exception as e:
+ except Exception as e:
         print(f"Error: ")
         print(e)
+
+def insertar_libro():
+     titulo = input("Escribe el titulo del nuevo libro: ")
+     autor = int(input("Escribe el id del autor: "))
+     isbn = input("Escribe el isbn del nuevo libro: ")
+     disponible = True 
+try:
+    
+        libro_dao = LibroDAO() 
+        id = libro_dao.obtener_ultimo_id() + 1
+        libro = Libro(id, titulo, autor, isbn, disponible)
+        libro_dao.insertar(libro) 
+        print("Insercion realizada con exito")
+except Exception as e:
+        print("Error al insertar un nuevo libro")
+        print(e) 
+
+
+
+def main():
+    print("=== BIBLIOTECA UNIVERSITARIA ===")
+    print("Menu de opciones")
+    print("1, Ver todos los libros")
+    print("2, Insertar un nuevo libro")
+    print("3, Actualizar un libro disponible")
+    print("4, Eliminar un libro disponible")
+    opcion = int(input("selecciona una opcion (1-4): "))
+
+    match opcion:
+         case 1:
+              ver_libros()
+         case 2:
+              insertar_libro()
+         case 3:
+              actualizar_libro()
+         case 4:
+              eliminar_libro()          
+
+
+
 
 if __name__ == "__main__":
     main()
